6ea6ea364b0dd8167f2f55919ef7e711fe7e0989
FatherDkay <paul.d.bailey@hotmail.com>
2022-01-12 14:56:11 -0800

    starting intro

---
+++ src/pages/Intro/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./introStyle.css"
+
+function Intro( props ) {
+
+  return (
+    <section>
+      <div class ="container">
+          <div class ="topArea">
+              Exactly right exists <br />
+              Lets build it together.
+          </div>
+          <div>
+          Welcome to my humble corner of cyberspace. Here, you’ll find the obsessions, passion pieces, and professional projects that have busied my mind and delighted my clients.
+          </div>
+      </div>
+    </section>
+  );
+}
+
+export default Intro;
