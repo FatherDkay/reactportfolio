130c24f20c14921da9c58a1fe6c1446ccff3172e
FatherDkay <paul.d.bailey@hotmail.com>
2022-01-12 15:14:44 -0800

    trying again

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import Header from "./components/Header"
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import ContactForm from './pages/Contact';
-
-
+import Intro from './pages/Intro';
 
 function App() {
   const [currentPage, setCurrentPage] = useState("About") //hook
   function renderPage () {
     if(currentPage==="About") {
       return <About />
+    } else if (currentPage==="Intro") {
+      return <Intro />
     } else if (currentPage==="Projects") {
       return <Projects />
     } else if (currentPage==="Contact") {
